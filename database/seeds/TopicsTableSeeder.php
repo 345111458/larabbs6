4f26b77a449a50ad28992fc502b15feff42f3277
vagrant <vagrant@homestead>
2019-10-24 07:12:59 +0000

    填充用户和话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,17 +2,28 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
+
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
+    	$faker = app(Faker\Generator::class);
 
+
+    	$user_ids = User::all()->pluck('id')->toArray();
+
+    	$category_id = Category::all()->pluck('id')->toArray();
+
+
+    	$topics = factory(Topic::class)->times(3000)->make()->each(function($topic) use ($faker,$user_ids,$category_id){
+
+    		$topic->user_id = $faker->randomElement($user_ids);
+    		$topic->category_id = $faker->randomElement($category_id);
+    	});
+        
         Topic::insert($topics->toArray());
     }
 
